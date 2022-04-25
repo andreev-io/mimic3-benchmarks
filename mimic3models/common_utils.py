0a85904bf5442e32edf8ba7f90407e3f2c5a4bd8
ilya <ilyaandreev2010@gmail.com>
2022-04-25 21:23:31 +0100

    Do not filter clinical variables and young patients

--- mimic3models/common_utils.py
+++ mimic3models/common_utils.py
@@ -15,8 +15,8 @@ def convert_to_dict(data, header, channel_info):
     for i in range(1, data.shape[1]):
         ret[i-1] = [(t, x) for (t, x) in zip(data[:, 0], data[:, i]) if x != ""]
         channel = header[i]
-        if len(channel_info[channel]['possible_values']) != 0:
-            ret[i-1] = list(map(lambda x: (x[0], channel_info[channel]['values'][x[1]]), ret[i-1]))
+        # if len(channel_info[channel]['possible_values']) != 0:
+            # ret[i-1] = list(map(lambda x: (x[0], channel_info[channel]['values'][x[1]]), ret[i-1]))
         ret[i-1] = list(map(lambda x: (float(x[0]), float(x[1])), ret[i-1]))
     return ret
 
